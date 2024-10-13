32efa158bd03dbe80451f87ed9410cbcc499a271
minyeong <minyk0512@gmail.com>
2024-10-14 04:48:30 +0900

    feat: style custom 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,29 @@ function App() {
 
   return (
     <>
-      <div style={{ display: 'flex', flexDirection: 'column', gap:'5px', justifyContent: 'center', alignItems: 'center' }}>
+      <div
+        style={{ display: 'flex', flexDirection: 'column', gap: '5px', justifyContent: 'center', alignItems: 'center' }}
+      >
+        <div>light 테마</div>
+        <RegisterForm
+          onSubmit={handleRegisterSubmit}
+          theme='light'
+        />
+        <div>dark 테마</div>
         <RegisterForm
           onSubmit={handleRegisterSubmit}
           theme='dark'
+          passwordCheck={true}
         />
-          <RegisterForm onSubmit={handleRegisterSubmit} theme='dark' passwordCheck={true}/>
+        <div>custom 테마</div>
+        <RegisterForm
+          onSubmit={handleRegisterSubmit}
+          theme='custom'
+          primaryColor='#bb4a4a'
+          textColor='white'
+          inputFieldColor='black'
+          inputTextColor='white'
+        />
       </div>
     </>
   );
--- src/components/RegisterForm.tsx
+++ src/components/RegisterForm.tsx
@@ -6,7 +6,11 @@ type RegisterFormPropType = {
   theme?: 'light' | 'dark' | 'custom';
   passwordCheck?: boolean;
   primaryColor?: string;
-  secondaryColor?: string;
+  titleColor?: string;
+  textColor?: string;
+  containerColor?: string;
+  inputFieldColor?: string;
+  inputTextColor?: string;
 };
 
 const RegisterForm = ({
@@ -14,7 +18,11 @@ const RegisterForm = ({
   theme = 'light',
   passwordCheck = false,
   primaryColor,
-  secondaryColor,
+  titleColor,
+  textColor,
+  containerColor,
+  inputFieldColor,
+  inputTextColor,
 }: RegisterFormPropType) => {
   const idRef = useRef<HTMLInputElement>(null);
   const passwordRef = useRef<HTMLInputElement>(null);
@@ -36,30 +44,37 @@ const RegisterForm = ({
   };
 
   return (
-    <div className='formContainer'>
-      <h3>회원가입</h3>
+    <div
+      className={`formContainer ${theme}`}
+      style={{ backgroundColor: containerColor }}
+    >
+      <h3 style={{ color: titleColor }}>회원가입</h3>
       <form onSubmit={handleSubmit}>
         <input
           ref={idRef}
           placeholder='아이디를 입력해주세요'
+          style={{ backgroundColor: inputFieldColor, color: inputTextColor }}
         />
         <input
           ref={passwordRef}
           type='password'
           placeholder='비밀번호를 입력해주세요'
+          style={{ backgroundColor: inputFieldColor, color: inputTextColor }}
         />
         {passwordCheck && (
           <input
             ref={passwordCheckRef}
             type='password'
             placeholder='비밀번호를 다시 한번 입력해주세요'
+            style={{ backgroundColor: inputFieldColor, color: inputTextColor }}
           />
         )}
         <input
           ref={nicknameRef}
           placeholder='닉네임을 입력해주세요'
+          style={{ backgroundColor: inputFieldColor, color: inputTextColor }}
         />
-        <button>가입하기</button>
+        <button style={{ backgroundColor: primaryColor, color: textColor }}>가입하기</button>
       </form>
     </div>
   );
